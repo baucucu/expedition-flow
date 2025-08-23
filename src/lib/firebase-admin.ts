5dc07f3d5cd74d2b499a2fa54221751fac84b4f4
Alexandru Raduca <alexandru.raduca@gmail.com>
2025-08-23 08:40:21 +0000

    you can use @/ path

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,20 +1,17 @@
 
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../expeditionflow-firebase-adminsdk.json';
 
-let app;
-
+// Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
-    try {
-        app = admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-            storageBucket: 'expeditionflow.appspot.com',
-        });
-    } catch (error) {
-        console.error('Firebase Admin initialization error', error);
-    }
-} else {
-    app = admin.app();
+  try {
+    const serviceAccount = require('../../expeditionflow-firebase-adminsdk.json');
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      storageBucket: 'expeditionflow.appspot.com',
+    });
+  } catch (error) {
+    console.error('Firebase Admin initialization error:', error);
+  }
 }
 
-export const adminApp = app;
+export const adminApp = admin.apps[0] ?? null;
