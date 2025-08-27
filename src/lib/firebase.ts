3559eacbce80979335830c72eb884ee09b4fcc4b
Alexandru Raduca <alexandru.raduca@gmail.com>
2025-08-27 13:05:41 +0000

    env vars changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  projectId: "expeditionflow",
-  appId: "1:108452888675:web:b799f8079ef451cab53856",
-  storageBucket: "expeditionflow.appspot.com",
-  apiKey: "AIzaSyB5_ZobHVwtOm1KimHLe4YTaTk46NujgDw",
-  authDomain: "expeditionflow.firebaseapp.com",
-  messagingSenderId: "108452888675",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 };
 
 // Initialize Firebase
